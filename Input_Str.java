b6d5d0b8c682b03702a6e8834b6c656d5c7aca43
Bhagyodaya Varshney <92192554+bhagyodaya@users.noreply.github.com>
2022-06-13 19:54:17 +0530

    Add files via upload

---
+++ Input_Str.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Input_Str{
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter:");
+        String str= sc.nextLine();
+        System.out.print(str);
+    }
+}
