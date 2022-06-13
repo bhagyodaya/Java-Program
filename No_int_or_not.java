e1ec22d2c10232cfc7f1f41a4e1c89b63a82192c
Bhagyodaya Varshney <92192554+bhagyodaya@users.noreply.github.com>
2022-06-13 20:28:50 +0530

    Add files via upload

---
+++ No_int_or_not.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class No_int_or_not{
+ public static void main(String[] args){
+		Scanner sc= new Scanner(System.in);
+		System.out.print("Enter No :");
+		boolean b1=sc.hasNextInt();
+		System.out.print(b1);
+	}
+}
